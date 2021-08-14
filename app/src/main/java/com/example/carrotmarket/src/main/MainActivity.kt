4cb47bfa55e00cbd15966f81efa71d379eb7495b
HYEWON <hiimhyewon@naver.com>
2021-08-14 23:47:37 +0900

    뼈대 레이아웃 구성 80%

---
+++ app/src/main/java/com/example/carrotmarket/src/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.carrotmarket.src.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.carrotmarket.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
