821a5d599d4607df183c042ce51dec81e668c2ea
HYEWON <hiimhyewon@naver.com>
2021-08-18 21:27:36 +0900

    signUp+detail+mypage

--- app/src/main/java/com/example/carrotmarket/src/config/src/product/ProductDetailActivity.kt
+++ app/src/main/java/com/example/carrotmarket/src/config/src/product/ProductDetailActivity.kt
@@ -1,16 +1,25 @@
 package com.example.carrotmarket.src.config.src.product
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Html
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.viewpager2.widget.ViewPager2
 import com.example.carrotmarket.R
 import com.example.carrotmarket.config.BaseActivity
 import com.example.carrotmarket.databinding.ActivityProductDetailsBinding
+import com.example.carrotmarket.src.MainActivity
+import com.example.carrotmarket.src.home.HomeFragment
+
 
 data class Other(
     val productDetailOtherImage: Int,
     val productDetailOtherName: String,
-    val productDetailOtherPrice: String
+    val productDetailOtherPrice: String,
 )
 
 val images = arrayOf(
@@ -28,20 +37,39 @@ class ProductDetailActivity :
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+        //top_icon
+        binding.detailImgTopBack.setOnClickListener {
+            intent =Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
+        //밑줄 넣기
+        binding.productDetailTxtUserTem.text = Html.fromHtml("<u>"+binding.productDetailTxtUserTem.text+"</u>")
+        binding.productDetailCate.text = Html.fromHtml("<u>"+binding.productDetailCate.text+"</u>")
+
+
+        //Recyclerview
         productDetailOtherAdapter= ProductDetailOtherAdapter(this, otherArrayList)
         binding.productDetailRvOthers.adapter=productDetailOtherAdapter
 
+        binding.productDetailRvOthers.layoutManager=GridLayoutManager(this,2)
+
+
+
+
+        //ViewPager2
         productDetailImageSliderAdapter= ProductDetailImageSliderAdapter(this,images)
         binding.productDetailVp2.adapter=productDetailImageSliderAdapter
-
-        binding.productDetailRvOthers.layoutManager=GridLayoutManager(this,2)
 
         binding.productDetailVp2.registerOnPageChangeCallback(object :ViewPager2.OnPageChangeCallback(){
 
             override fun onPageScrolled(
                 position: Int,
                 positionOffset: Float,
-                positionOffsetPixels: Int
+                positionOffsetPixels: Int,
             ) {
                 super.onPageScrolled(position, positionOffset, positionOffsetPixels)
 
@@ -49,10 +77,15 @@ class ProductDetailActivity :
 
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
-
+                setCurrentIndicator(position)
             }
         })
+        setupIndicators(images.size)
 
+
+
+
+        //더미데이터
         otherArrayList.add(
             Other(
                 R.drawable.item_image,
@@ -69,6 +102,39 @@ class ProductDetailActivity :
             )
         )
 
+    }
+
+    private fun setupIndicators(count: Int) {
+        val indicators: Array<ImageView?> = arrayOfNulls<ImageView>(count)
+        val params = LinearLayout.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        params.setMargins(14, 8, 14, 8)
+        for (i in indicators.indices) {
+            indicators[i] = ImageView(this)
+            indicators[i]!!.setImageDrawable(ContextCompat.getDrawable(this,
+                R.drawable.detail_vp2_indicator_inactive))
+            indicators[i]!!.setLayoutParams(params)
+            binding.detailLlIndicator.addView(indicators[i])
+        }
+        setCurrentIndicator(0)
+    }
+
+    private fun setCurrentIndicator(position: Int) {
+        val childCount: Int =  binding.detailLlIndicator.getChildCount()
+        for (i in 0 until childCount) {
+            val imageView: ImageView =  binding.detailLlIndicator.getChildAt(i) as ImageView
+            if (i == position) {
+                imageView.setImageDrawable(ContextCompat.getDrawable(
+                    this,
+                    R.drawable.detail_vp2_indicator_active
+                ))
+            } else {
+                imageView.setImageDrawable(ContextCompat.getDrawable(
+                    this,
+                    R.drawable.detail_vp2_indicator_inactive
+                ))
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/carrotmarket/src/config/src/users/SignUpMainActivity.kt
+++ app/src/main/java/com/example/carrotmarket/src/config/src/users/SignUpMainActivity.kt
@@ -1,9 +1,17 @@
 package com.example.carrotmarket.src.users
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import com.example.carrotmarket.config.BaseActivity
 import com.example.carrotmarket.databinding.ActivitySignUpMainBinding
+import java.util.*
+import kotlin.concurrent.timer
+
 
 class SignUpMainActivity :
     BaseActivity<ActivitySignUpMainBinding>(ActivitySignUpMainBinding::inflate) {
@@ -12,15 +20,87 @@ class SignUpMainActivity :
         super.onCreate(savedInstanceState)
 
 
-        binding.signUpMainStartBtn.setOnClickListener {
+        //TextWatcher를 사용하여 실시간 입력 동작 확인
+        binding.signUpMainEdtId.addTextChangedListener(object : TextWatcher {
+            //입력하기 전
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
 
-            intent = Intent(this, SignUpProfileActivity::class.java)
-            intent.putExtra("userID",binding.signUpMainEdtId.text.toString())
-            intent.putExtra("password",binding.signUpMainEdtPwd.text.toString())
-            startActivity(intent)
-            finish()
+            //변화가 있을 때
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (binding.signUpMainEdtId.length() > 3) {
+                    binding.signUpMainBtnNext.setCardBackgroundColor(Color.parseColor("#4d5158"))
 
+                    // 비밀번호 입력하기 버튼을 누르면
+                    binding.signUpMainBtnNext.setOnClickListener {
+                        binding.signUpMainMainLl.visibility = View.GONE
+                        binding.signUpMainTxtModi.visibility = View.INVISIBLE
+                        binding.signUpMainTxtFoundId.visibility = View.INVISIBLE
+                        binding.signUpMainEdtPwd.visibility = View.VISIBLE
+                        binding.signUpMainTxtWarn.visibility = View.VISIBLE
+                        binding.signUpMainPwTimer.visibility = View.VISIBLE
+                        binding.signUpBtnPwConfirm.visibility = View.VISIBLE
+                        binding.signUpMainBtnNext.setCardBackgroundColor(Color.parseColor("#4d5158"))
+
+                        //타이머 시작
+                        startTimer()
+
+                    }
+
+                } else {
+                    binding.signUpMainBtnNext.setCardBackgroundColor(Color.parseColor("#FFdcdee2"))
+                }
+            }
+
+            //입력이 끝나면
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
+        binding.signUpMainEdtPwd.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (binding.signUpMainEdtPwd.length() > 3) {
+                    binding.signUpBtnPwConfirm.setCardBackgroundColor(Color.parseColor("#FFee8548"))
+                } else if (binding.signUpMainEdtPwd.length() == 0) {
+                    binding.signUpBtnPwConfirm.setCardBackgroundColor(Color.parseColor("#FFdcdee2"))
+
+                    binding.signUpBtnPwConfirm.setOnClickListener {
+                        intent = Intent(this@SignUpMainActivity, SignUpProfileActivity::class.java)
+                        intent.putExtra("userID", binding.signUpMainEdtId.text.toString())
+                        intent.putExtra("password", binding.signUpMainEdtPwd.text.toString())
+                        startActivity(intent)
+                        finish()
+                    }
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
+    }
+
+    // 타이머
+    private var time = 300
+    private var timerTask: Timer? = null
+    private fun startTimer() {
+        timerTask = timer(period = 1000) {
+
+            time--
+
+            val min = time / 60
+            val sec = time - (60 * min)
+
+            runOnUiThread {
+                binding.signUpMainPwTimer.text = "${min}분 ${sec}초"
+
+                // 0초가 되면 멈추도록
+                if (time == 0) {
+                    timerTask?.cancel()
+                }
+            }
         }
-
     }
 }
